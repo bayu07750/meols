98cd360e58878af8aeb149846f2bf04127b9d1b5
bayu07750 <bayu07750@gmail.com>
2021-09-02 01:01:16 +0700

    responsive pada header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,15 +6,21 @@ const Header = () => {
   return (
     <header className="h-[400px] overflow-hidden relative">
       <div className="relative z-10 h-full w-full flex flex-col items-center justify-center">
-        <h1 className="text-5xl text-white font-extrabold">Temukan makanan atau sekedar melihat</h1>
-        <div className="text-white mt-4 max-w-xl text-center">
+        <h1 className="text-white font-extrabold text-center text-4xl sm:text-4xl lg:text-5xl sm:max-w-2xl lg:max-w-none px-4 sm:px-0">
+          Temukan makanan atau sekedar melihat
+        </h1>
+        <div className="hidden lg:block text-white mt-4 max-w-xl text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem odio accusantium sunt tempora ut
           sapiente optio, adipisci fugit. Accusantium vero officia optio a assumenda, sunt reprehenderit illum sapiente
           fugit?
         </div>
         <SearchMealsForm />
       </div>
-      <img src={headerImage} alt="image header" className="absolute inset-0 blur-sm object-center" />
+      <img
+        src={headerImage}
+        alt="image header"
+        className="absolute top-0 left-0 h-full w-full filter blur-sm object-cover object-center"
+      />
     </header>
   );
 };
--- src/components/header/SearchMealsForm.jsx
+++ src/components/header/SearchMealsForm.jsx
@@ -6,14 +6,14 @@ const SearchMealsForm = () => {
   const { enteredSearchQuery, handleChangeEnteredSearchQuery, handleKeyUpEnter } = useMeals();
 
   return (
-    <div className="min-w-[500px] mt-12 flex items-center gap-2 bg-white px-6 py-3 rounded-full">
+    <div className="w-[300px] sm:w-[500px] lg:w-[700px] mt-12 flex items-center gap-2 bg-white px-6 py-3 rounded-full">
       <SearchIcon className="w-6 h-6" />
       <input
         type="search"
         id="search"
         name="search"
         placeholder="Search meals here..."
-        className="border-none focus:outline-none focus:ring-0 w-full"
+        className="px-2 py-0 border-none focus:outline-none focus:ring-0 w-full"
         value={enteredSearchQuery}
         onChange={handleChangeEnteredSearchQuery}
         onKeyUp={handleKeyUpEnter}

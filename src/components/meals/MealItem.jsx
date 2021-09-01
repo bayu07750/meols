596108b0d66a1af88be301fdedd9686c847ab61d
bayu07750 <bayu07750@gmail.com>
2021-09-02 00:29:59 +0700

    change shadow from shadow-md to shadow-lg on mealitem

--- src/components/meals/MealItem.jsx
+++ src/components/meals/MealItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MealItem = ({ title, country, thumbnail }) => {
   return (
-    <div className="overflow-hidden rounded-lg shadow-md">
+    <div className="overflow-hidden rounded-lg shadow-xl">
       <img src={thumbnail} alt="" className="h-48 w-full object-cover object-center" />
       <div className="p-6">
         <h2 className="text-2xl font-semibold tracking-wide">{title}</h2>
--- src/components/meals/Meals.jsx
+++ src/components/meals/Meals.jsx
@@ -5,7 +5,7 @@ import { MealsContext } from './../../context/meals-context';
 const Meals = () => {
   const mealsCtx = useContext(MealsContext);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
       {mealsCtx.meals.map((meal) => {
         return <MealItem key={meal.id} title={meal.title} country={meal.country} thumbnail={meal.thumbnail} />;
       })}

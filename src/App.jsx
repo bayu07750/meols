b9ebe2bd3565c3a1b5b9b6d56c5d34e8b1c1deee
bayu07750 <bayu07750@gmail.com>
2021-09-01 14:48:36 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="selection:bg-red-500 selection:text-white">Let's get started</h1>
+    </div>
+  );
+}
+
+export default App;

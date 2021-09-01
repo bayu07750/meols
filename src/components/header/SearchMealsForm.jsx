901ef53f344f11cfaa4cb2b6357eaf21a2cdfe1f
bayu07750 <bayu07750@gmail.com>
2021-09-02 01:04:11 +0700

    change padding on search form from py-3 py-4

--- src/components/header/SearchMealsForm.jsx
+++ src/components/header/SearchMealsForm.jsx
@@ -6,7 +6,7 @@ const SearchMealsForm = () => {
   const { enteredSearchQuery, handleChangeEnteredSearchQuery, handleKeyUpEnter } = useMeals();
 
   return (
-    <div className="w-[300px] sm:w-[500px] lg:w-[700px] mt-12 flex items-center gap-2 bg-white px-6 py-3 rounded-full">
+    <div className="w-[300px] sm:w-[500px] lg:w-[700px] mt-12 flex items-center gap-2 bg-white px-6 py-4 rounded-full">
       <SearchIcon className="w-6 h-6" />
       <input
         type="search"
